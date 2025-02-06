af1f8cd361091668830023cf69f8af9e457eabed
alexgarciamartinez <122872080+alexgarciamartinez@users.noreply.github.com>
2025-02-06 13:34:16 +0000

    feat: bash de creacion de grupos y usuarios

---
+++ ensayos/alex/creaciongrupoyusuarios.sh
@@ -0,0 +1,22 @@
+#\!/usr/bin/bash
+sudo groupadd ventas
+sudo groupadd marketing
+sudo groupadd desarrollo
+sudo useradd -g ventas cristhian
+sudo useradd -g ventas khadiya
+sudo useradd -g ventas andrik
+sudo useradd -g ventas guillem
+
+sudo useradd -g marketing Danib
+sudo useradd -g marketing Didac
+sudo useradd -g marketing Danio
+sudo useradd -g marketing joanmanel
+
+sudo useradd -g desarrollo Hector
+sudo useradd -g desarrollo Willy
+sudo useradd -g desarrollo Alex
+sudo useradd -g desarrollo Eric
+sudo useradd -g desarrollo Erik
+sudo useradd -g desarrollo Pol
+
+
